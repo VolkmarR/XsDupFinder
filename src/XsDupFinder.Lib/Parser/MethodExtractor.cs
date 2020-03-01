6aa705547d4bb22aa8f5dec7367767730f3acf63
volkmar <volkmar.rigo@infominds.eu>
2020-03-01 16:53:21 +0100

    Setup new solution

---
+++ src/XsDupFinder.Lib/Parser/MethodExtractor.cs
@@ -0,0 +1,47 @@
+﻿using LanguageService.CodeAnalysis.XSharp.SyntaxParser;
+using LanguageService.SyntaxTree;
+using LanguageService.SyntaxTree.Misc;
+using LanguageService.SyntaxTree.Tree;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XsDupFinder.Lib.Parser
+{
+    public class MethodExtractor
+    {
+        class MethodListener : XSharpBaseListener
+        {
+            public override void EnterMethod([NotNull] XSharpParser.MethodContext context)
+            {
+                foreach (var methodtypeContext in context.methodtype())
+                {
+                    Console.WriteLine(methodtypeContext.GetText());
+                }
+
+                Console.WriteLine(context.Sig.GetText());
+
+                Console.WriteLine(context.statementBlock().GetText());
+            }
+        }
+
+        public void Execute(string fileName)
+        {
+            var lexer = XSharpLexer.Create(File.ReadAllText(fileName), fileName);
+            lexer.RemoveErrorListeners();
+
+            var tokenStream = new CommonTokenStream(lexer, 0);
+            var parser = new XSharpParser(tokenStream);
+            parser.RemoveErrorListeners();
+
+            var source = parser.source();
+            var listener = new MethodListener();
+
+            new ParseTreeWalker().Walk(listener, source);
+        }
+
+    }
+}
---
+++ src/XsDupFinderCmd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XsDupFinder.Lib.Parser;
+
+namespace XsDupFinderCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MethodExtractor().Execute(@"..\..\..\..\assets\TestData\simpleFile.prg ");
+            Console.ReadKey();
+        }
+    }
+}
