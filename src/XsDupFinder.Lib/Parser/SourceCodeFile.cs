4160e27996fcc72806561167be891f5ee823b378
volkmar <volkmar.rigo@infominds.eu>
2020-03-01 17:39:28 +0100

    New class SourceCodeFile

---
+++ src/XsDupFinder.Lib/Parser/SourceCodeFile.cs
@@ -0,0 +1,29 @@
+﻿using Extensions.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XsDupFinder.Lib.Parser
+{
+    public class SourceCodeFile
+    {
+        public string FileName { get; private set; }
+        public string SourceCode { get; private set; }
+        public uint HashCode { get; private set; }
+
+        public SourceCodeFile(string fileName)
+        {
+            FileName = fileName;
+            SourceCode = null;
+            HashCode = 0;
+            if (File.Exists(fileName))
+            {
+                SourceCode = File.ReadAllText(fileName);
+                HashCode = XXHash.XXH32(Encoding.UTF8.GetBytes(SourceCode));
+            }
+        }
+    }
+}
