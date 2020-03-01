e6190d1b25f0e6a5727b82423746b44e38ebf38c
volkmar <volkmar.rigo@infominds.eu>
2020-03-01 17:05:14 +0100

    Data classes for MethodExtract

---
+++ src/XsDupFinder.Lib/Parser/MethodInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XsDupFinder.Lib.Parser
+{
+    class MethodInfo
+    {
+        public string Name { get; set; }
+        public List<StatementInfo> StatementList { get; set; } = new List<StatementInfo>();
+    }
+}
