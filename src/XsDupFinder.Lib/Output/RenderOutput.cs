5a47422f4b4d48a7b65f9b2844f597536a0b253f
volkmar <volkmar.rigo@infominds.eu>
2020-03-08 18:11:56 +0100

    Added RenderOutput and RenderJson

---
+++ src/XsDupFinder.Lib/Output/RenderOutput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XsDupFinder.Lib.Common;
+using XsDupFinder.Lib.Finder;
+
+namespace XsDupFinder.Lib.Output
+{
+    public class RenderOutput
+    {
+        readonly Configuration Configuration;
+        readonly List<Duplicate> Duplicates;
+        readonly List<IRender> Renders;
+
+        public RenderOutput(Configuration configuration, List<Duplicate> duplicates)
+        {
+            Duplicates = duplicates;
+            Configuration = configuration;
+            Renders = new List<IRender> { new RenderJson() };
+        }
+
+        public void Execute()
+        {
+            foreach (var render in Renders)
+            {
+                render.Execute(Configuration, Duplicates);
+            }
+        }
+    }
+}
