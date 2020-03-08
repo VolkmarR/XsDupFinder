554e42de172435897db4e91254da9194b0938178
volkmar <volkmar.rigo@infominds.eu>
2020-03-08 12:38:36 +0100

    Added CacheDB

---
+++ src/XsDupFinder.Tests/Cache/CacheDBTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XsDupFinder.Lib.Cache;
+using XsDupFinder.Lib.Parser;
+using Xunit;
+using FluentAssertions;
+
+namespace XsDupFinder.Tests.Cache
+{
+    public class CacheDBTests
+    {
+        string GetTempDBName() => Path.ChangeExtension(Path.GetTempFileName(), ".DB");
+
+        CodeInfo BuildDummyCodeInfo(SourceCodeFile sourceCodeFile)
+            => new CodeInfo
+            {
+                FileName = sourceCodeFile.FileName,
+                HashCode = sourceCodeFile.HashCode,
+                MethodList = new List<MethodInfo>
+                    {
+                        new MethodInfo
+                        {
+                            Name = "test", StatementList = new List<StatementInfo>
+                            {
+                                new StatementInfo
+                                {
+                                    Start = 1,
+                                    End = 10,
+                                    StartLine = 1, Hashcode = 999 } } } }
+            };
+
+
+        [Fact]
+        public void NotFoundInsertFound()
+        {
+            var dbName = GetTempDBName();
+
+            using (var cache = new CacheDB(dbName))
+            {
+                var sourceCodeFile = new SourceCodeFile("XXX", "test");
+
+                cache.TryGetValue(sourceCodeFile, out var codeInfo).Should().Be(false);
+
+                var newCodeInfo = BuildDummyCodeInfo(sourceCodeFile);
+
+                cache.Add(newCodeInfo);
+
+                cache.TryGetValue(sourceCodeFile, out codeInfo).Should().Be(true);
+
+                codeInfo.Should().BeEquivalentTo(newCodeInfo);
+            }
+
+            File.Delete(dbName);
+        }
+    }
+}
+
