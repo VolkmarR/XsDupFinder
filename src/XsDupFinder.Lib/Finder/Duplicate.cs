8ed97ec0a14f8b79bbcaea5052cd9e8b14a79d0d
volkmar <volkmar.rigo@infominds.eu>
2020-03-08 17:22:00 +0100

    Added DuplicateFinder with tests

---
+++ src/XsDupFinder.Lib/Finder/Duplicate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XsDupFinder.Lib.Finder
+{
+    public class Duplicate
+    {
+        public class Location
+        {
+            public string Filename { get; set; }
+            public string MethodName { get; set; }
+            public int StartLine { get; set; }
+            public int EndLine { get; set; }
+            public int PercentOfMethod { get; set; }
+        }
+
+        public int LineCount { get => Locations.Count > 0 ? Locations[0].EndLine - Locations[0].StartLine + 1 : 0; }
+        public string Code { get; set; }
+        public List<Location> Locations { get; set; } = new List<Location>();
+    }
+}
