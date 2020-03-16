1a13bc8a05788750539f003bca8e04e510d198d9
Volkmar Rigo <volkmar@rigo.bz>
2020-03-16 22:28:40 +0100

    Added config file option

---
+++ src/XsDupFinderCmd/ConfigurationCmd.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XsDupFinder.Lib.Common;
+
+namespace XsDupFinderCmd
+{
+    class ConfigurationCmd : Configuration
+    {
+        [Option('c', "ConfgFile", SetName = "Config File", HelpText = "Configuration file", Required = true)]
+        public string ConfgFile { get; set; }
+    }
+}
