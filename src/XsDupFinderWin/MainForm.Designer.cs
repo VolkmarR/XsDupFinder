a71d02e618213c69ad247af10ff25a9065188d98
volkmar <volkmar.rigo@infominds.eu>
2020-03-22 20:00:12 +0100

    Load, Save and Start

--- src/XsDupFinderWin/MainForm.Designer.cs
+++ src/XsDupFinderWin/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace XsDupFinderWin
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,80 +29,85 @@
         private void InitializeComponent()
         {
             this.TopPanel = new System.Windows.Forms.Panel();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SaveConfigButton = new System.Windows.Forms.Button();
+            this.LoadConfigButton = new System.Windows.Forms.Button();
             this.ConfigGroupBox = new System.Windows.Forms.GroupBox();
+            this.MinLineForFullMethodDuplicateCheckEdit = new System.Windows.Forms.NumericUpDown();
+            this.MinLineForDuplicateEdit = new System.Windows.Forms.NumericUpDown();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.CacheFileNameEdit = new System.Windows.Forms.TextBox();
+            this.OutputDirectoryEdit = new System.Windows.Forms.TextBox();
+            this.SourceDirectoryEdit = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.LogPanel = new System.Windows.Forms.Panel();
-            this.AnalysisProgress = new System.Windows.Forms.ProgressBar();
-            this.label6 = new System.Windows.Forms.Label();
+            this.OpenConfigFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.AnalysisLog = new System.Windows.Forms.TextBox();
+            this.LogPanel = new System.Windows.Forms.Panel();
+            this.SaveConfigFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.TopPanel.SuspendLayout();
             this.ConfigGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MinLineForFullMethodDuplicateCheckEdit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinLineForDuplicateEdit)).BeginInit();
             this.LogPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // TopPanel
             // 
-            this.TopPanel.Controls.Add(this.button6);
-            this.TopPanel.Controls.Add(this.button5);
-            this.TopPanel.Controls.Add(this.button4);
+            this.TopPanel.Controls.Add(this.StartButton);
+            this.TopPanel.Controls.Add(this.SaveConfigButton);
+            this.TopPanel.Controls.Add(this.LoadConfigButton);
             this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.TopPanel.Location = new System.Drawing.Point(4, 4);
             this.TopPanel.Name = "TopPanel";
-            this.TopPanel.Size = new System.Drawing.Size(792, 36);
+            this.TopPanel.Size = new System.Drawing.Size(955, 36);
             this.TopPanel.TabIndex = 1;
             // 
-            // button6
+            // StartButton
             // 
-            this.button6.Location = new System.Drawing.Point(699, 6);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(89, 23);
-            this.button6.TabIndex = 2;
-            this.button6.Text = "Start analysis";
-            this.button6.UseVisualStyleBackColor = true;
+            this.StartButton.Location = new System.Drawing.Point(699, 6);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(89, 23);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "Start analysis";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
             // 
-            // button5
+            // SaveConfigButton
             // 
-            this.button5.Location = new System.Drawing.Point(104, 6);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(97, 23);
-            this.button5.TabIndex = 1;
-            this.button5.Text = "Save settings";
-            this.button5.UseVisualStyleBackColor = true;
+            this.SaveConfigButton.Location = new System.Drawing.Point(104, 6);
+            this.SaveConfigButton.Name = "SaveConfigButton";
+            this.SaveConfigButton.Size = new System.Drawing.Size(97, 23);
+            this.SaveConfigButton.TabIndex = 1;
+            this.SaveConfigButton.Text = "Save settings";
+            this.SaveConfigButton.UseVisualStyleBackColor = true;
+            this.SaveConfigButton.Click += new System.EventHandler(this.SaveConfigButton_Click);
             // 
-            // button4
+            // LoadConfigButton
             // 
-            this.button4.Location = new System.Drawing.Point(3, 6);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(95, 23);
-            this.button4.TabIndex = 0;
-            this.button4.Text = "Load settings";
-            this.button4.UseVisualStyleBackColor = true;
+            this.LoadConfigButton.Location = new System.Drawing.Point(3, 6);
+            this.LoadConfigButton.Name = "LoadConfigButton";
+            this.LoadConfigButton.Size = new System.Drawing.Size(95, 23);
+            this.LoadConfigButton.TabIndex = 0;
+            this.LoadConfigButton.Text = "Load settings";
+            this.LoadConfigButton.UseVisualStyleBackColor = true;
+            this.LoadConfigButton.Click += new System.EventHandler(this.LoadConfigButton_Click);
             // 
             // ConfigGroupBox
             // 
+            this.ConfigGroupBox.Controls.Add(this.MinLineForFullMethodDuplicateCheckEdit);
+            this.ConfigGroupBox.Controls.Add(this.MinLineForDuplicateEdit);
             this.ConfigGroupBox.Controls.Add(this.button3);
             this.ConfigGroupBox.Controls.Add(this.button2);
             this.ConfigGroupBox.Controls.Add(this.button1);
-            this.ConfigGroupBox.Controls.Add(this.textBox5);
-            this.ConfigGroupBox.Controls.Add(this.textBox4);
-            this.ConfigGroupBox.Controls.Add(this.textBox3);
-            this.ConfigGroupBox.Controls.Add(this.textBox2);
-            this.ConfigGroupBox.Controls.Add(this.textBox1);
+            this.ConfigGroupBox.Controls.Add(this.CacheFileNameEdit);
+            this.ConfigGroupBox.Controls.Add(this.OutputDirectoryEdit);
+            this.ConfigGroupBox.Controls.Add(this.SourceDirectoryEdit);
             this.ConfigGroupBox.Controls.Add(this.label5);
             this.ConfigGroupBox.Controls.Add(this.label4);
             this.ConfigGroupBox.Controls.Add(this.label3);
@@ -111,10 +116,24 @@
             this.ConfigGroupBox.Dock = System.Windows.Forms.DockStyle.Top;
             this.ConfigGroupBox.Location = new System.Drawing.Point(4, 40);
             this.ConfigGroupBox.Name = "ConfigGroupBox";
-            this.ConfigGroupBox.Size = new System.Drawing.Size(792, 156);
+            this.ConfigGroupBox.Size = new System.Drawing.Size(955, 156);
             this.ConfigGroupBox.TabIndex = 2;
             this.ConfigGroupBox.TabStop = false;
             this.ConfigGroupBox.Text = "Settings";
+            // 
+            // MinLineForFullMethodDuplicateCheckEdit
+            // 
+            this.MinLineForFullMethodDuplicateCheckEdit.Location = new System.Drawing.Point(197, 124);
+            this.MinLineForFullMethodDuplicateCheckEdit.Name = "MinLineForFullMethodDuplicateCheckEdit";
+            this.MinLineForFullMethodDuplicateCheckEdit.Size = new System.Drawing.Size(73, 20);
+            this.MinLineForFullMethodDuplicateCheckEdit.TabIndex = 27;
+            // 
+            // MinLineForDuplicateEdit
+            // 
+            this.MinLineForDuplicateEdit.Location = new System.Drawing.Point(197, 98);
+            this.MinLineForDuplicateEdit.Name = "MinLineForDuplicateEdit";
+            this.MinLineForDuplicateEdit.Size = new System.Drawing.Size(73, 20);
+            this.MinLineForDuplicateEdit.TabIndex = 26;
             // 
             // button3
             // 
@@ -122,6 +141,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(25, 20);
             this.button3.TabIndex = 25;
+            this.button3.TabStop = false;
             this.button3.Text = "...";
             this.button3.UseVisualStyleBackColor = true;
             // 
@@ -131,6 +151,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(25, 20);
             this.button2.TabIndex = 24;
+            this.button2.TabStop = false;
             this.button2.Text = "...";
             this.button2.UseVisualStyleBackColor = true;
             // 
@@ -140,43 +161,30 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(25, 20);
             this.button1.TabIndex = 23;
+            this.button1.TabStop = false;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // textBox5
+            // CacheFileNameEdit
             // 
-            this.textBox5.Location = new System.Drawing.Point(197, 123);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(53, 20);
-            this.textBox5.TabIndex = 22;
+            this.CacheFileNameEdit.Location = new System.Drawing.Point(197, 71);
+            this.CacheFileNameEdit.Name = "CacheFileNameEdit";
+            this.CacheFileNameEdit.Size = new System.Drawing.Size(562, 20);
+            this.CacheFileNameEdit.TabIndex = 20;
             // 
-            // textBox4
+            // OutputDirectoryEdit
             // 
-            this.textBox4.Location = new System.Drawing.Point(197, 97);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(53, 20);
-            this.textBox4.TabIndex = 21;
+            this.OutputDirectoryEdit.Location = new System.Drawing.Point(197, 45);
+            this.OutputDirectoryEdit.Name = "OutputDirectoryEdit";
+            this.OutputDirectoryEdit.Size = new System.Drawing.Size(562, 20);
+            this.OutputDirectoryEdit.TabIndex = 19;
             // 
-            // textBox3
+            // SourceDirectoryEdit
             // 
-            this.textBox3.Location = new System.Drawing.Point(197, 71);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(562, 20);
-            this.textBox3.TabIndex = 20;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(197, 45);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(562, 20);
-            this.textBox2.TabIndex = 19;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(197, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(562, 20);
-            this.textBox1.TabIndex = 18;
+            this.SourceDirectoryEdit.Location = new System.Drawing.Point(197, 19);
+            this.SourceDirectoryEdit.Name = "SourceDirectoryEdit";
+            this.SourceDirectoryEdit.Size = new System.Drawing.Size(562, 20);
+            this.SourceDirectoryEdit.TabIndex = 18;
             // 
             // label5
             // 
@@ -223,58 +231,56 @@
             this.label1.TabIndex = 13;
             this.label1.Text = "Source code directory";
             // 
-            // LogPanel
+            // OpenConfigFileDialog
             // 
-            this.LogPanel.Controls.Add(this.AnalysisLog);
-            this.LogPanel.Controls.Add(this.label6);
-            this.LogPanel.Controls.Add(this.AnalysisProgress);
-            this.LogPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LogPanel.Location = new System.Drawing.Point(4, 196);
-            this.LogPanel.Name = "LogPanel";
-            this.LogPanel.Padding = new System.Windows.Forms.Padding(8);
-            this.LogPanel.Size = new System.Drawing.Size(792, 234);
-            this.LogPanel.TabIndex = 4;
-            // 
-            // AnalysisProgress
-            // 
-            this.AnalysisProgress.Dock = System.Windows.Forms.DockStyle.Top;
-            this.AnalysisProgress.Location = new System.Drawing.Point(8, 8);
-            this.AnalysisProgress.Name = "AnalysisProgress";
-            this.AnalysisProgress.Size = new System.Drawing.Size(776, 23);
-            this.AnalysisProgress.TabIndex = 4;
-            // 
-            // label6
-            // 
-            this.label6.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label6.Location = new System.Drawing.Point(8, 31);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(776, 4);
-            this.label6.TabIndex = 5;
+            this.OpenConfigFileDialog.DefaultExt = "yaml";
+            this.OpenConfigFileDialog.Filter = "XsDupeFinder Settings|*.yaml";
+            this.OpenConfigFileDialog.Title = "Load settings";
             // 
             // AnalysisLog
             // 
             this.AnalysisLog.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.AnalysisLog.Location = new System.Drawing.Point(8, 35);
+            this.AnalysisLog.Location = new System.Drawing.Point(8, 8);
             this.AnalysisLog.Multiline = true;
             this.AnalysisLog.Name = "AnalysisLog";
             this.AnalysisLog.ReadOnly = true;
-            this.AnalysisLog.Size = new System.Drawing.Size(776, 191);
+            this.AnalysisLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.AnalysisLog.Size = new System.Drawing.Size(939, 318);
             this.AnalysisLog.TabIndex = 6;
             // 
-            // Form1
+            // LogPanel
+            // 
+            this.LogPanel.Controls.Add(this.AnalysisLog);
+            this.LogPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LogPanel.Location = new System.Drawing.Point(4, 196);
+            this.LogPanel.Name = "LogPanel";
+            this.LogPanel.Padding = new System.Windows.Forms.Padding(8);
+            this.LogPanel.Size = new System.Drawing.Size(955, 334);
+            this.LogPanel.TabIndex = 4;
+            // 
+            // SaveConfigFileDialog
+            // 
+            this.SaveConfigFileDialog.DefaultExt = "yaml";
+            this.SaveConfigFileDialog.Filter = "XsDupeFinder Settings|*.yaml";
+            this.SaveConfigFileDialog.Title = "Save settings";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 434);
+            this.ClientSize = new System.Drawing.Size(963, 534);
             this.Controls.Add(this.LogPanel);
             this.Controls.Add(this.ConfigGroupBox);
             this.Controls.Add(this.TopPanel);
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Padding = new System.Windows.Forms.Padding(4);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "XsDupeFinder";
             this.TopPanel.ResumeLayout(false);
             this.ConfigGroupBox.ResumeLayout(false);
             this.ConfigGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MinLineForFullMethodDuplicateCheckEdit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinLineForDuplicateEdit)).EndInit();
             this.LogPanel.ResumeLayout(false);
             this.LogPanel.PerformLayout();
             this.ResumeLayout(false);
@@ -284,27 +290,27 @@
         #endregion
 
         private System.Windows.Forms.Panel TopPanel;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button SaveConfigButton;
+        private System.Windows.Forms.Button LoadConfigButton;
         private System.Windows.Forms.GroupBox ConfigGroupBox;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox CacheFileNameEdit;
+        private System.Windows.Forms.TextBox OutputDirectoryEdit;
+        private System.Windows.Forms.TextBox SourceDirectoryEdit;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel LogPanel;
+        private System.Windows.Forms.NumericUpDown MinLineForFullMethodDuplicateCheckEdit;
+        private System.Windows.Forms.NumericUpDown MinLineForDuplicateEdit;
+        private System.Windows.Forms.OpenFileDialog OpenConfigFileDialog;
         private System.Windows.Forms.TextBox AnalysisLog;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ProgressBar AnalysisProgress;
+        private System.Windows.Forms.Panel LogPanel;
+        private System.Windows.Forms.SaveFileDialog SaveConfigFileDialog;
     }
 }
 
--- src/XsDupFinderWin/MainForm.cs
+++ src/XsDupFinderWin/MainForm.cs
@@ -7,14 +7,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using XsDupFinder.Lib.Common;
+using XsDupFinder.Lib.Finder;
+using XsDupFinder.Lib.Output;
 
 namespace XsDupFinderWin
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private string LastConfigName = "";
+
+        private void SaveConfigToForm(Configuration config)
+        {
+            SourceDirectoryEdit.Text = config.SourceDirectory;
+            OutputDirectoryEdit.Text = config.OutputDirectory;
+            CacheFileNameEdit.Text = config.CacheFileName;
+            MinLineForDuplicateEdit.Value = config.MinLineForDuplicate;
+            MinLineForFullMethodDuplicateCheckEdit.Value = config.MinLineForFullMethodDuplicateCheck;
+        }
+
+        private Configuration LoadConfigFromForm()
+            => new Configuration()
+            {
+                SourceDirectory = SourceDirectoryEdit.Text,
+                OutputDirectory = OutputDirectoryEdit.Text,
+                CacheFileName = CacheFileNameEdit.Text,
+                MinLineForDuplicate = (int)MinLineForDuplicateEdit.Value,
+                MinLineForFullMethodDuplicateCheck = (int)MinLineForFullMethodDuplicateCheckEdit.Value
+            };
+
+        private void ClearOutputLog()
+            => AnalysisLog.Clear();
+
+        private void UpdateOutputLog(string msg)
+            => Invoke((MethodInvoker)(() =>
+            {
+                AnalysisLog.AppendText(msg + Environment.NewLine);
+                AnalysisLog.Refresh();
+            }));
+
+        private void LoadConfigButton_Click(object sender, EventArgs e)
+        {
+            if (OpenConfigFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            LastConfigName = OpenConfigFileDialog.FileName;
+            SaveConfigToForm(Configuration.Load(LastConfigName));
+        }
+
+        private void SaveConfigButton_Click(object sender, EventArgs e)
+        {
+            SaveConfigFileDialog.FileName = LastConfigName;
+            if (SaveConfigFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            LoadConfigFromForm().SaveConfig(SaveConfigFileDialog.FileName);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            var config = LoadConfigFromForm().FixOptionalValues();
+            ClearOutputLog();
+
+            var duplicateFinder = new DirectoryDuplicateFinder(config, UpdateOutputLog);
+            new RenderOutput(config, duplicateFinder.Execute()).Execute();
         }
     }
 }
