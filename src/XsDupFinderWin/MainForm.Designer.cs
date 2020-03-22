4bc7e5cbf695ece7a00e6ccce0c5a3cd3e98a516
volkmar <volkmar.rigo@infominds.eu>
2020-03-22 19:18:29 +0100

    First layout of mainform

---
+++ src/XsDupFinderWin/MainForm.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace XsDupFinderWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ConfigGroupBox = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LogPanel = new System.Windows.Forms.Panel();
+            this.AnalysisProgress = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.AnalysisLog = new System.Windows.Forms.TextBox();
+            this.TopPanel.SuspendLayout();
+            this.ConfigGroupBox.SuspendLayout();
+            this.LogPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.Controls.Add(this.button6);
+            this.TopPanel.Controls.Add(this.button5);
+            this.TopPanel.Controls.Add(this.button4);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(4, 4);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(792, 36);
+            this.TopPanel.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(699, 6);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(89, 23);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Start analysis";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(104, 6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Save settings";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Load settings";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // ConfigGroupBox
+            // 
+            this.ConfigGroupBox.Controls.Add(this.button3);
+            this.ConfigGroupBox.Controls.Add(this.button2);
+            this.ConfigGroupBox.Controls.Add(this.button1);
+            this.ConfigGroupBox.Controls.Add(this.textBox5);
+            this.ConfigGroupBox.Controls.Add(this.textBox4);
+            this.ConfigGroupBox.Controls.Add(this.textBox3);
+            this.ConfigGroupBox.Controls.Add(this.textBox2);
+            this.ConfigGroupBox.Controls.Add(this.textBox1);
+            this.ConfigGroupBox.Controls.Add(this.label5);
+            this.ConfigGroupBox.Controls.Add(this.label4);
+            this.ConfigGroupBox.Controls.Add(this.label3);
+            this.ConfigGroupBox.Controls.Add(this.label2);
+            this.ConfigGroupBox.Controls.Add(this.label1);
+            this.ConfigGroupBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ConfigGroupBox.Location = new System.Drawing.Point(4, 40);
+            this.ConfigGroupBox.Name = "ConfigGroupBox";
+            this.ConfigGroupBox.Size = new System.Drawing.Size(792, 156);
+            this.ConfigGroupBox.TabIndex = 2;
+            this.ConfigGroupBox.TabStop = false;
+            this.ConfigGroupBox.Text = "Settings";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(761, 71);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 20);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(761, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 20);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(761, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 20);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(197, 123);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(53, 20);
+            this.textBox5.TabIndex = 22;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(197, 97);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(53, 20);
+            this.textBox4.TabIndex = 21;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(197, 71);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(562, 20);
+            this.textBox3.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(562, 20);
+            this.textBox2.TabIndex = 19;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(562, 20);
+            this.textBox1.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Min. lines for duplicate method";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Min. lines for duplicate fragment";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Cache filename";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Output directory";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Source code directory";
+            // 
+            // LogPanel
+            // 
+            this.LogPanel.Controls.Add(this.AnalysisLog);
+            this.LogPanel.Controls.Add(this.label6);
+            this.LogPanel.Controls.Add(this.AnalysisProgress);
+            this.LogPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LogPanel.Location = new System.Drawing.Point(4, 196);
+            this.LogPanel.Name = "LogPanel";
+            this.LogPanel.Padding = new System.Windows.Forms.Padding(8);
+            this.LogPanel.Size = new System.Drawing.Size(792, 234);
+            this.LogPanel.TabIndex = 4;
+            // 
+            // AnalysisProgress
+            // 
+            this.AnalysisProgress.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AnalysisProgress.Location = new System.Drawing.Point(8, 8);
+            this.AnalysisProgress.Name = "AnalysisProgress";
+            this.AnalysisProgress.Size = new System.Drawing.Size(776, 23);
+            this.AnalysisProgress.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label6.Location = new System.Drawing.Point(8, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(776, 4);
+            this.label6.TabIndex = 5;
+            // 
+            // AnalysisLog
+            // 
+            this.AnalysisLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AnalysisLog.Location = new System.Drawing.Point(8, 35);
+            this.AnalysisLog.Multiline = true;
+            this.AnalysisLog.Name = "AnalysisLog";
+            this.AnalysisLog.ReadOnly = true;
+            this.AnalysisLog.Size = new System.Drawing.Size(776, 191);
+            this.AnalysisLog.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 434);
+            this.Controls.Add(this.LogPanel);
+            this.Controls.Add(this.ConfigGroupBox);
+            this.Controls.Add(this.TopPanel);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(4);
+            this.Text = "XsDupeFinder";
+            this.TopPanel.ResumeLayout(false);
+            this.ConfigGroupBox.ResumeLayout(false);
+            this.ConfigGroupBox.PerformLayout();
+            this.LogPanel.ResumeLayout(false);
+            this.LogPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox ConfigGroupBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel LogPanel;
+        private System.Windows.Forms.TextBox AnalysisLog;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ProgressBar AnalysisProgress;
+    }
+}
+
