5ce469990e62d9657360dba0f6aa5073fd56d6f4
Volkmar Rigo <volkmar.rigo@infominds.eu>
2020-10-11 15:54:46 +0200

    Changed fileformat of duplicates.json file

---
+++ src/XsDupFinder.Lib/Output/Json/JsonOutput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XsDupFinder.Lib.Common;
+using XsDupFinder.Lib.Finder;
+
+namespace XsDupFinder.Lib.Output.Json
+{
+    class JsonOutput
+    {
+        public int Version { get; set; } = 1;
+        public Configuration Configuration { get; set; }
+        public List<Duplicate> Duplicates { get; set; }
+    }
+}
--- src/XsDupFinder.Lib/Output/RenderJson.cs
+++ src/XsDupFinder.Lib/Output/RenderJson.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using XsDupFinder.Lib.Common;
 using XsDupFinder.Lib.Finder;
+using XsDupFinder.Lib.Output.Json;
 
 namespace XsDupFinder.Lib.Output
 {
     class RenderJson : IRender
     {
         public void Execute(Configuration configuration, List<Duplicate> duplicates)
-            => RenderFileHelper.SaveRenderOutput(configuration, "duplicates.json", JsonConvert.SerializeObject(duplicates, Formatting.Indented));
+        {
+            var data = new JsonOutput { Configuration = configuration, Duplicates = duplicates };
+            RenderFileHelper.SaveRenderOutput(configuration, "duplicates.json", JsonConvert.SerializeObject(data, Formatting.Indented));
+        }
     }
 }
